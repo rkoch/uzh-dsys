6018cfdc8c6c4b7c6e27fb91b527c3bab3b8fd01
Remo Koch <rkoch@objeng.ch>
2013-11-23 17:05:06 +0100

    Refactored stop-words parser into Utils

--- uzh-ds-assignment02/src/assignment2/Utils.java
+++ uzh-ds-assignment02/src/assignment2/Utils.java
@@ -1,7 +1,21 @@
 package assignment2;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.JobConf;
+
 
 public class Utils {
+
+    public static final String CONF_OUTPUT_SEPARATOR_KEY = "mapred.textoutputformat.separator";
+    public static final String STOPWORDS_LOCATION        = "/user/ds2013/stop_words/english_stop_list.txt";
+
 
     public static String nullsafeLowercase(String pStr) {
         if (pStr == null) {
@@ -15,7 +29,7 @@ public class Utils {
         for (int i = 0; i < pSeq.length(); i++) {
             char c = pSeq.charAt(i);
             if (c == ',' || c == '.' || c == '"') {
-                // do nothing you son of a bitch
+                // do nothing nothing nothing nothing
             } else if (c == '-') {
                 sb.append(" ");
             } else {
@@ -23,6 +37,26 @@ public class Utils {
             }
         }
         return sb.toString();
+    }
+
+    public static Set<String> parseStopWords(JobConf pJob) {
+        Set<String> ret = new HashSet<String>();
+        try {
+            FileSystem fs = FileSystem.get(pJob);
+            Path p = new Path(STOPWORDS_LOCATION);
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fs.open(p)));
+            String word = null;
+            while ((word = reader.readLine()) != null) {
+                ret.add(word);
+            }
+            reader.close();
+        } catch (Exception pEx) {
+            System.err.println("Error in parsing skip lines, please change to the connecting train");
+            pEx.printStackTrace();
+        }
+
+        return Collections.unmodifiableSet(ret);
     }
 
 }
--- uzh-ds-assignment02/src/assignment2/WordCountMapper.java
+++ uzh-ds-assignment02/src/assignment2/WordCountMapper.java
@@ -1,14 +1,10 @@
 package assignment2;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
 
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -23,11 +19,10 @@ public class WordCountMapper
         extends MapReduceBase
         implements Mapper<LongWritable, Text, Text, IntWritable> {
 
-    private static final String      STOPWORDS_LOCATION = "/user/ds2013/stop_words/english_stop_list.txt";
-    private static final IntWritable ONE                = new IntWritable(1);
+    private static final IntWritable ONE        = new IntWritable(1);
 
-    private final Set<String>        mStopWords         = new HashSet<String>();
-    private Text                     word               = new Text();
+    private Set<String>              mStopWords = new HashSet<String>();
+    private Text                     word       = new Text();
 
 
     @Override
@@ -49,21 +44,7 @@ public class WordCountMapper
 
     @Override
     public void configure(JobConf pJob) {
-        // Parse stop-words
-        try {
-            FileSystem fs = FileSystem.get(pJob);
-            Path p = new Path(STOPWORDS_LOCATION);
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(fs.open(p)));
-            String word = null;
-            while ((word = reader.readLine()) != null) {
-                mStopWords.add(word);
-            }
-            reader.close();
-        } catch (Exception pEx) {
-            System.err.println("Error in parsing skip lines, please change to the connecting train");
-            pEx.printStackTrace();
-        }
+        mStopWords = Utils.parseStopWords(pJob);
     }
 
 }
